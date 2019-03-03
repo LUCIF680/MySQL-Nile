da354500558891a335496340b5eba748b8f336fa
Pratik Mazumdar <45979935+LUCIF680@users.noreply.github.com>
2019-03-04 00:18:28 +0530

    V1.1

--- MySQLNile.php
+++ MySQLNile.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'DataBase.php';
 /*
 How to use : -
 First update your server information in DataBase.php file.
@@ -6,19 +7,18 @@ First update your server information in DataBase.php file.
 Created by Pratik Mazumdar(LUCIF680) haki version.
 MySQL Nile is a PDO class extended, you can edit, use this API as you like. You can use this API commercial and non-commercial use.
 */
-class MySQLNile extends DataBase{
-
-  /* inserting data without sql injection
+class MySQLNile implements DataBase{
+/* inserting data without sql injection
 $input variable will input any data you want to input should be in string
 $table_coloum is the coloum name
 $table_name is the name of the table you want to insert data into
   */
-  public static function insertData($input,$table_coloum,$table_name){
+  public function insertData($input,$table_coloum,$table_name){
     //can input your server information with the help of DataBase.php
-     $servername = MySQLNile::$servername;
-     $dbname = MySQLNile::$dbname;
-     $password_database = MySQLNile::$password_database;
-     $username_database = MySQLNile::$username_database;
+     $servername = MySQLNile::servername;
+     $dbname = MySQLNile::dbname;
+     $password_database = MySQLNile::password_database;
+     $username_database = MySQLNile::username_database;
      $input = explode(",",$input);
      //converting "ram,shyam,raghu" to ":ram,:shyam,:raghu"
      $parameter_value = explode(",",$table_coloum);
@@ -38,45 +38,37 @@ $table_name is the name of the table you want to insert data into
      $parameter =  explode(",",$table_coloum_value);
      //Uploading DATA TO DATABASE
          try {
-
              $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username_database, $password_database);
              $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
              $sql = "INSERT INTO `".$table_name."` ($table_coloum)
                              VALUES ($table_coloum_value)"; //will be written in  ":ram,:shyam,:raghu"
              $query =$conn->prepare($sql);
-
              for($i=0;$i<count($parameter_value);$i++)
                  $query->bindParam($parameter[$i], $input[$i]);
-
              $query->execute();
          }
-         catch(PDOException $e)
-         {
+         catch(PDOException $e) {
              echo $e->getMessage();
          }
          $conn = null;
-
  }
-
  /* for any querry writing
  $query should be string*/
- public static function query($query){
-     $servername = MySQLNile::$servername;
-     $dbname = MySQLNile::$dbname;
-     $password_database = MySQLNile::$password_database;
-     $username_database = MySQLNile::$username_database;
+ public function query($query){
+     $servername = MySQLNile::servername;
+     $dbname = MySQLNile::dbname;
+     $password_database = MySQLNile::password_database;
+     $username_database = MySQLNile::username_database;
      try {
        $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username_database  , $password_database);
        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
        $sql = "".$query;
        $query =$conn->prepare($sql);
        $query->execute();
-         }
-     catch(PDOException $e)
-     {
-        echo $e->getMessage();
      }
-
+     catch(PDOException $e){
+         echo $e->getMessage();
+     }
      $conn = null;
  }
 
@@ -85,11 +77,11 @@ $table_name is the name of the table you want to insert data into
 In $table_coloums input your coloums name and their values
  */
 
- public static function createTable($table_coloums,$table_name){
-     $servername = MySQLNile::$servername;
-     $dbname = MySQLNile::$dbname;
-     $password_database = MySQLNile::$password_database;
-     $username_database = MySQLNile::$username_database;
+ public function createTable($table_coloums,$table_name){
+     $servername = MySQLNile::servername;
+     $dbname = MySQLNile::dbname;
+     $password_database = MySQLNile::password_database;
+     $username_database = MySQLNile::username_database;
      try {
          $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username_database, $password_database);
          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -100,15 +92,12 @@ In $table_coloums input your coloums name and their values
      }
      $conn = null;
  }
-
-/*parameter will be the query you want to input
-
-*/
- public static function fetch($string){
-     $servername = MySQLNile::$servername;
-     $dbname = MySQLNile::$dbname;
-     $password_database = MySQLNile::$password_database;
-     $username_database = MySQLNile::$username_database;
+/*parameter will be the query you want to input*/
+ public function fetch($string){
+     $servername = MySQLNile::servername;
+     $dbname = MySQLNile::dbname;
+     $password_database = MySQLNile::password_database;
+     $username_database = MySQLNile::username_database;
      try {
          $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username_database  , $password_database);
          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -116,12 +105,12 @@ In $table_coloums input your coloums name and their values
          $query->execute();
          $result = $query->setFetchMode(PDO::FETCH_ASSOC);
          $result = $query->fetchAll();
+         $conn = null;
          return $result;
      }catch(PDOException $e){
-        echo $e->getMessage();
-     }
-     $conn = null;
- }
+         $conn = null;
+        return $e->getMessage();
 
+     }
+ }
 }
-?>
