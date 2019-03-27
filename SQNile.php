a8749e141909cf700722b4000094ca5eb74c45d6
Pratik Mazumdar <45979935+LUCIF680@users.noreply.github.com>
2019-03-27 14:01:46 +0530

    1.3v

--- SQNile.php
+++ SQNile.php
@@ -6,10 +6,7 @@ class SQNile{
     function  __construct(){
         try {
             if (func_num_args() > 4)
-                throw new Exception('Argument Error');
-        } catch (Exception $e) {
-            echo $e->getMessage();
-        }
+                throw new Exception('SQNile Exception: Argument Error');
         switch (func_num_args()) {
             case 4:
                 $this->password_database = func_get_arg(3);
@@ -20,43 +17,94 @@ class SQNile{
             case 1:
                 $this->dbname = func_get_arg(0);
         }
-        $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username_database, $this->password_database);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username_database, $this->password_database);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        }
+        catch(PDOException $e){echo $e->getMessage();}
+        catch (Exception $e) {echo $e->getMessage();}
     }
-    function fetchAll($query,$array){
+    function databaseInfo(){
+        $this->conn = null;
         try {
-            $query = $this->conn->prepare($query);
-            $query->execute($array);
+            if (func_num_args() > 4)
+                throw new Exception('SQNile Exception: Argument Error');
+            switch (func_num_args()) {
+                case 4:
+                    $this->password_database = func_get_arg(3);
+                case 3:
+                    $this->username_database = func_get_arg(2);
+                case 2:
+                    $this->servername = func_get_arg(1);
+                case 1:
+                    $this->dbname = func_get_arg(0);
+            }
+            $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username_database, $this->password_database);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        }
+        catch(PDOException $e){echo $e->getMessage();}
+        catch (Exception $e) {echo $e->getMessage();}
+    }
+    function fetchAll(){
+        try {
+            if (func_num_args() > 2 || func_num_args() == 0)
+                throw new Exception('SQNile Exception: Argument Error');
+            $query = $this->conn->prepare(func_get_arg(0));
+            switch(func_num_args()){
+                case 1:
+                    $query->execute();
+                    break;
+                case 2:
+                    $query->execute(func_get_arg(1));
+            }
             $result = $query->setFetchMode(PDO::FETCH_ASSOC);
             $result = $query->fetchAll();
             return $result;
-        }catch(PDOException $e){
-            return $e->getMessage();
         }
+        catch(PDOException $e) {echo $e->getMessage();}
+        catch (Exception $e) {echo $e->getMessage();}
     }
-    function fetch($query,$array){
-       $result = $this->fetchAll($query,$array);
-       if (isset($result[0])){
-           return $result[0];
-       }else
-           return $result;
-    }
-    function query($query,$array){
-        try{
-            $query = $this->conn->prepare($query);
-            $query->execute($array);
-        }catch(PDOException $e){
-            echo $e->getMessage();
+    function fetch(){
+        try {
+            if (func_num_args() > 2 || func_num_args() == 0)
+                throw new Exception('SQNile Exception: Argument Error');
+            switch (func_num_args()) {
+                case 1:
+                    $result = $this->fetchAll(func_get_arg(0));
+                    break;
+                case 2:
+                    $result = $this->fetchAll(func_get_arg(0),func_get_arg(1));
+            }
+            if (isset($result[0])) {
+                return $result[0];
+            } else
+                return $result;
         }
+        catch(PDOException $e) {echo $e->getMessage();}
+        catch (Exception $e) {echo $e->getMessage();}
+    }
+    function query(){
+        try {
+            if (func_num_args() > 2 || func_num_args() == 0)
+                throw new Exception('SQNile Exception: Argument Error');
+            $query = $this->conn->prepare(func_get_arg(0));
+            switch(func_num_args()){
+                case 1:
+                $query->execute();
+                break;
+                case 2:
+                $query->execute(func_get_arg(1));
+            }
+        }
+        catch(PDOException $e) {echo $e->getMessage();}
+        catch (Exception $e) {echo $e->getMessage();}
     }
     function createTable($tableName,$columnInfo){
         try{
             $sql = "CREATE TABLE IF NOT EXISTS`".$tableName."` ( ".$columnInfo." )";
             $this->conn->exec($sql);
-        }catch(PDOException $e){
-            echo $e->getMessage();
-        }
+        }catch(PDOException $e){echo $e->getMessage();}
     }
     function __destruct(){
         $this->conn = null;
